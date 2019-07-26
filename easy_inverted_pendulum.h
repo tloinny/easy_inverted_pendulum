2a941b3aae8fd8715fc5c804d1beec6fa11f2158
tloinny <tloinny@gmail.com>
2019-07-26 10:44:29 +0800

    2019.7.26 10:44 debug

--- easy_inverted_pendulum.h
+++ easy_inverted_pendulum.h
@@ -5,7 +5,6 @@ class inverted_pendulum
 {
 public:
 	inverted_pendulum(float A_Kp, float A_Kd, float P_Kp, float P_Kd, int A_limit, int P_limit);
-	~inverted_pendulum();
 	void Angle_loop(float Angle_kp, float Angle_kd);
 	void Position_loop(float Position_kp, float Position_kd);
 	void SetAngleSetPoint(float setpoint);
